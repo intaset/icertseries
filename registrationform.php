f92150328a06837360c6ee6e28b23821aa50b5dc
administrator@international-aset.com <ICNFAicepr1120>
2018-03-20 11:05:22 -0400

    Added important dates

--- registrationform.php
+++ registrationform.php
@@ -185,7 +185,7 @@ mail($your_email,$your_subject,$message,$your_headers);
 </div>
         <div class="bg">
           <h1>3<sup>rd</sup> International Conference on Energy<br>Research and Technology (ICERT'19)</h1>
-          <p class="subhead">August, 2019 | Prague, Czech Republic</p>
+          <p class="subhead">August 18 - 20, 2019 | Prague, Czech Republic</p>
 
           <a href="../papers" class="bg-link">Submissions</a> <p class="dot">&middot;</p> <a href="../dates" class="bg-link">Important Dates</a> <p class="dot">&middot;</p> <a href="../registration" class="bg-link">Registration</a>
 
@@ -213,7 +213,7 @@ mail($your_email,$your_subject,$message,$your_headers);
 
         <div class="bg">
           <h1>3<sup>rd</sup> International Conference on Energy<br>Research and Technology (ICERT'19)</h1>
-          <p class="subhead">August, 2019 | Prague, Czech Republic</p>
+          <p class="subhead">August 18 - 20, 2019 | Prague, Czech Republic</p>
 
           <a href="../papers" class="bg-link">Submissions</a> <p class="dot">&middot;</p> <a href="../dates" class="bg-link">Important Dates</a> <p class="dot">&middot;</p> <a href="../registration" class="bg-link">Registration</a>
 
