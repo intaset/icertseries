17dad61f651439aa0aad2ab6dda205028e093882
administrator@international-aset.com <>
2018-05-29 10:45:48 -0400

    Changed conference location

--- _site/contactus.php
+++ _site/contactus.php
@@ -103,7 +103,7 @@ if (isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response
 </div>
         <div class="bg">
           <h1>3<sup>rd</sup> International Conference on Energy<br>Research and Technology (ICERT'19)</h1>
-          <p class="subhead">August 18 - 20, 2019 | Prague, Czech Republic</p>
+          <p class="subhead">August 18 - 20, 2019 | Lisbon, Portugal</p>
 
           <a href="../papers" class="bg-link">Submissions</a> <p class="dot">&middot;</p> <a href="../dates" class="bg-link">Important Dates</a> <p class="dot">&middot;</p> <a href="../registration" class="bg-link">Registration</a>
 
@@ -131,7 +131,7 @@ if (isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response
 
         <div class="bg">
           <h1>3<sup>rd</sup>International Conference on Energy<br>Research and Technology (ICERT'19)</h1>
-          <p class="subhead">August 18 - 20, 2019 | Prague, Czech Republic</p>
+          <p class="subhead">August 18 - 20, 2019 | Lisbon, Portugal</p>
 
           <a href="../papers" class="bg-link">Submissions</a> <p class="dot">&middot;</p> <a href="../dates" class="bg-link">Important Dates</a> <p class="dot">&middot;</p> <a href="../registration" class="bg-link">Registration</a>
 
